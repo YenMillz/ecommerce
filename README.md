009882a9e0b4d3758c311af5e4383b5807d499a3
YenMillz <bivol.gabriel9@gmail.com>
2022-03-23 20:06:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ecommerce-laravel-project
+
+## Features
+
+-   Fully functional E-commerce website front-end and back-end built from scratch.
+-   Using laravel voyager as an admin panel for the site.
+-   javascript, jquery, bootstrap and css for the front-end.
+-   Intelligent searching mechanism for products.
+-   Awesome Cart package that uses session.
+-   An artisan command to seed the database with all necessary dummy data, even for voyager tables (php artisan ecommerce:install).
+-   Different user roles and privileges.
+-   Categories, tags and price filtering for easier search for products.
+-   And much more features.
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="bg-dark">
+    <div class="container bg-dark">
+        <div class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="footer-link">Privacy policy</a><br>
+                    <a href="#" class="footer-link">Term and conditions</a><br>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Welcome to Asterix
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, assumenda. Culpa, libero.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Contact:
+                    </h3>
+                    <p>Phone: +000000000</p>
+                    <p>Email: bivol.gabriel@gmail.com</p>
+                    <p>Github: <a href="https://github.com/YenMillz">@yenmillz</a></p>
+                    <p>LinkedIn: <a href="https://www.linkedin.com/in/gavril-bivol-b0b6561a9/">Bivol Gvaril</a></p>
+                </div>
+            </div>
+        </div>
+        <p>copyright &copy; Bivol Gavril - All rights reserved 2022</p>
+    </div>
+</footer>
